4b0b0e5a69504d917726fe07f02fcac7611a4c44
John Pawlak <johnpawlak@gmail.com>
2018-09-29 22:47:21 -0400

    optimize functions and reorganize folders and files

--- js/Main.js
+++ js/Main.js
@@ -4,11 +4,12 @@ window.onload = function() {
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
 
-	colorRect(0,0,canvas.width, canvas.clientHeight, 'black')
-	colorText('Loading Images', canvas.width/2, canvas.height/2, 'white');
+	colorRect(0,0, canvas.width,canvas.height, 'black');
+	colorText("LOADING IMAGES", canvas.width/2, canvas.height/2, 'white');
 
 	loadImages();
 }
+
 function imageLoadingDoneSoStartGame() {
 	var framesPerSecond = 30;
 	setInterval(updateAll, 1000/framesPerSecond);
